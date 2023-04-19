c428de3d4059ccc094546654836877f965b072b9
alotofmilk <munzimelo@ewhain.net>
2023-04-19 17:18:03 +0900

    accounts 기능 구현

--- pjt/urls.py
+++ pjt/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 # 이미지 업로드
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
