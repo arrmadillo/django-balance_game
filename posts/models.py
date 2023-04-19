15cf13509d12b0f965b2925391eb188cac2b7579
armadillo <dltngks54@gmail.com>
2023-04-19 16:45:22 +0900

    모델 작성

---
+++ posts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+class Post(models.Model):
+    title           = models.CharField(max_length=20)
+    user            = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    like_users      = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_posts')
+    select1_content = models.TextField(max_length=200)
+    select1_image   = models.ImageField(blank=True, upload_to='images/')
+    select2_content = models.TextField(max_length=200)
+    select2_image   = models.ImageField(blank=True, upload_to='images/')
+    select1_user    = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='select1_post')
+    select2_user    = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='select2_post')
+    created_at      = models.DateTimeField(auto_now_add=True)
+    updated_at      = models.DateTimeField(auto_now=True)
