6a726746de50ef33a61624a0f4a445b33e49d7dc
armadillo <dltngks54@gmail.com>
2023-04-20 10:10:30 +0900

    form 작성!

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Post
-
+from .forms import PostForm
 
 # Create your views here.
 
@@ -17,3 +18,26 @@ def detail(request, post_pk):
         'post': post,
     }
     return render(request, 'posts/detail.html', context)
+
+@login_required
+def create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            # 저장 x, 인스턴스만 생성
+            post = form.save(commit=False)
+            post.user = request.user
+            # 저장
+            post.save()
+            return redirect('posts:detail', post.pk)
+    else:
+        form = PostForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'posts/create.html', context)
+
+# @login_requireds
+# def select(request, post_pk):
+#     post = Post.objects.get(pk=post_pk)
+    
